85375035ad46d5ddd8ce63c27b1702b1ccaae77f
ThifannyS <127845293+ThifannyS@users.noreply.github.com>
2024-08-29 01:27:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const parte1= "Olá, tudo bem?";
+const primeiroNome ="Thifanny";
+const sobrenome ="Pereira";
+const idade= 17;
+const altura= 1.63;
+const massa= 58;
+const cidadeNatal="São José dos Pinhais";
+const meuDia= "Eu vou á academia de manhã, estudo de tarde e de noite vou para a escola";
+const sobreMim= "Eu amo ler, ouvir música, assistir séries, viajar e estar com as pessoas que amo";
+
+console.log(parte1);
+console.log(primeiroNome);
+console.log(sobrenome);
+console.log(idade);
+console.log(altura);
+console.log(massa);
+console.log(cidadeNatal);
+console.log(meuDia);
+console.log(sobreMim);
